21ada05ae8d171c9bd5bc856e446aeabc69b74cf
sagisaka2 <sagisaka2>
2017-12-21 02:10:30 +0900

    schedulerの作成を行なった

--- src/main/java/com/spring/app/controller/periodic/JobPeriodical.java
+++ src/main/java/com/spring/app/controller/periodic/JobPeriodical.java
@@ -1,37 +1,35 @@
 package com.spring.app.controller.periodic;
 
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.spring.app.LoggingFilter;
 import com.spring.app.model.LogProduct;
 import com.spring.app.service.LogProductsService;
 
+@Component
 public class JobPeriodical {
 
 	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
-	
-	//1日一回実行
-	public void oneDayJob(LogProductsService logProductService){
-		TimerTask task = new TimerTask() {
-			public void run() {
-				//登録したログデータからアウトプットする
-				List<LogProduct> logProducts = logProductService.findByCheckOutput(false);
-				if(logProducts.isEmpty()){
-					logger.info("Nothing");
-				}else{
-					logProductService.updateCheckOutput(logProducts);
-					logger.info("Output Data");
-				}
-			}
-		};
-		Timer timer = new Timer();
-		//1日
-		timer.schedule(task, 0, 60000*60*24);
-	}
 
+	@Autowired
+	private LogProductsService logProductService;
+
+	//0時に一回実行
+	@Scheduled(cron = "0 0 0 * * *", zone = "Asia/Tokyo")
+	public void oneDayJob(){
+		//登録したログデータからアウトプットする
+		List<LogProduct> logProducts = logProductService.findByCheckOutput(false);
+		if(logProducts.isEmpty()){
+			logger.info("Nothing");
+		}else{
+			logProductService.updateCheckOutput(logProducts);
+			logger.info("Output Data");
+		}
+	}
 }
